a0eb137d875b13fb8243d8ca3e22c747b07e8ded
Fork Man <forkman@biome.sh>
2021-09-25 07:24:30 +0000

    Backport habitat/main at ce4a94ce6cab8979e63adb636e6b020fd6f01c5e

--- test/end-to-end/test_studio_can_build_packages.ps1
+++ test/end-to-end/test_studio_can_build_packages.ps1
@@ -1,3 +1,6 @@
+[Diagnostics.CodeAnalysis.SuppressMessage("PSUseCorrectCasing", '')]
+param ()
+
 # `build` is a built-in helper function that maps to `bio pkg exec biome/bio-plan-build`
 # rather than `bio pkg build` to avoid 'studio-in-studio' situations. Verify that the
 # command functions. We assume that if the build succeeds (exits 0) we've passed this
