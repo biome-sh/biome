f99a9b74b7643bf1c1555d844274e200b5028cb4
Yauhen Artsiukhou <jsirex@gmail.com>
2022-03-17 19:55:05 +0400

    Use podman as container builder

--- components/rootless_studio/build.sh
+++ components/rootless_studio/build.sh
@@ -3,19 +3,21 @@
 BIO_VERSION=$(cat ../../VERSION)
 PACKAGE_TARGET=x86_64-linux
 
-docker build \
+BUILDER=podman
+
+$BUILDER build \
        -t biomesh/bio-"$PACKAGE_TARGET":"$BIO_VERSION" \
        --no-cache \
        --build-arg BIO_VERSION="$BIO_VERSION" \
        --build-arg PACKAGE_TARGET="$PACKAGE_TARGET" \
        .
 
-docker build \
+$BUILDER build \
        -t biomesh/default-studio-"$PACKAGE_TARGET":"$BIO_VERSION" \
        --no-cache \
        --build-arg BIO_VERSION="$BIO_VERSION" \
        --build-arg PACKAGE_TARGET="$PACKAGE_TARGET" \
        ./default
 
-docker push biomesh/bio-"$PACKAGE_TARGET":"$BIO_VERSION"
-docker push biomesh/default-studio-"$PACKAGE_TARGET":"$BIO_VERSION"
+$BUILDER push biomesh/bio-"$PACKAGE_TARGET":"$BIO_VERSION"
+$BUILDER push biomesh/default-studio-"$PACKAGE_TARGET":"$BIO_VERSION"
