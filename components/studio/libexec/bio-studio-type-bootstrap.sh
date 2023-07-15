12e710faf4985628d1daa1014e87ccd647a52278
Fork Man <forkman@biome.sh>
2023-07-15 07:22:31 +0000

    Backport habitat/main at 461255fc1b048694e5b5f2596a938f12599f7309

--- components/studio/libexec/bio-studio-type-bootstrap.sh
+++ components/studio/libexec/bio-studio-type-bootstrap.sh
@@ -26,7 +26,7 @@ finish_setup() {
       # Import the secret origin key, required for signing packages
       info "Importing '$key' secret origin key"
       # shellcheck disable=2154
-      if key_text=$($bio origin key export --type secret "$key"); then
+      if key_text=$(HAB_LICENSE="$HAB_LICENSE" $bio origin key export --type secret "$key"); then
         printf -- "%s" "${key_text}" | _bio origin key import
       else
         echo "Error exporting $key key"
@@ -52,7 +52,7 @@ finish_setup() {
       fi
       # Attempt to import the public origin key, which can be used for local
       # package installations where the key may not yet be uploaded.
-      if key_text=$($bio origin key export --type public "$key" 2>/dev/null); then
+      if key_text=$(HAB_LICENSE="$HAB_LICENSE" $bio origin key export --type public "$key" 2>/dev/null); then
         info "Importing '$key' public origin key"
         printf -- "%s" "${key_text}" | _bio origin key import
       else
@@ -172,7 +172,8 @@ _bio() (
   # We remove a couple of env vars we do not want for this instance of the studio
   unset HAB_CACHE_KEY_PATH
   unset HAB_BLDR_CHANNEL
-  $bb env FS_ROOT="$HAB_STUDIO_ROOT" "$bio" "$@"
+  # Set the HAB_LICENSE because the license accepted files don't yet exist on the chroot filesystem
+  $bb env FS_ROOT="$HAB_STUDIO_ROOT" HAB_LICENSE="$HAB_LICENSE" "$bio" "$@"
 )
 
 _pkgpath_for() {
