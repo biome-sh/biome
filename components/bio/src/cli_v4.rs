775b3aec4ff50883d730355c45d078ab70ef76a4
Fork Man <forkman@biome.sh>
2026-09-05 11:15:50 +0000

    Backport habitat/main at 9d9bf316bb59d40997688eeaab4d894b836b8957

--- components/bio/src/cli_v4.rs
+++ components/bio/src/cli_v4.rs
@@ -108,7 +108,7 @@ enum Bio {
         target_os = "macos",
         any(
             all(target_os = "linux", any(target_arch = "x86_64", target_arch = "aarch64")),
-            all(target_os = "windows", target_arch = "x86_64")
+            all(target_os = "windows", any(target_arch = "x86_64", target_arch = "aarch64"))
         )
     ))]
     #[command(name = "studio")]
